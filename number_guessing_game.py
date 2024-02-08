71f37c510d50c8ae86a716f1bcddecc2393ef56a
SirPopGhost <srikardevesetti@gmail.com>
2024-02-07 17:37:10 -0800

    Number guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,25 @@
 import random
-num = random.randint(1, 100)
+num = random.randint(1, 2)
+user = 0
+user_count = 0
+while user != num:
+    user_count += 1
+    user = input("Enter your guess: ")
+    user = int(user)
 
-user = input("Enter your guess: ")
-user = int(user)
+    if user > num:
+        print("Too high, it was")
+    elif user < num:
+        print("Too low, it was")
+    else:
+        print("You got it!")
+        print(f"You have guessed {user_count} amount of times.")
+        input_user = input("Do you want to go again? Type y if yes. ")
+        if input_user == "y":
+            num = random.randint(1, 100)
+            user = 0
+            user_count = 0
+        else:
+            print("Goodbye!")
+            break
 
-if user > num:
-    print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
